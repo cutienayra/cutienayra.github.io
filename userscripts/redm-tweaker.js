aca91b8091a5ff24a87c91724c76cff2fb3d3083
Nayra* - Lilac Pillow <nayracutie7@proton.me>
2025-01-01 16:30:14 +0000

    Create redm-tweaker.js

---
+++ userscripts/redm-tweaker.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         RemovedEDM Tweaker~
+// @version      v1.2A
+// @description  Making the best out of RemovedEDM :3
+// @match        https://removededm.com/*
+// @match        https://www.removededm.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle ( `
+    h1.home-header__title > span#RemovedEDM,
+    .citizen-preferences,
+    a[title="Random page"],
+    div.home-header>a[href="/Special:Search"] {
+  display: none!important;
+}
+h1.home-header__title::before {
+  content: '"removed.edm"';
+}
+h1.home-header__title {
+text-transform: none !important;
+font-weight: normal !important;
+}
+a.new{
+color: var(--color-link-red)!important;
+}
+` );
+
+})();
