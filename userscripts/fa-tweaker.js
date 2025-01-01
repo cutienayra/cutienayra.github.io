1fbb0b389fb20faf1fa8c1203762fc98e48860fa
Nayra* - Lilac Pillow <nayracutie7@proton.me>
2025-01-01 16:47:45 +0000

    Update fa-tweaker.js

--- userscripts/fa-tweaker.js
+++ userscripts/fa-tweaker.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         FurAffinity Tweaker~
-// @version      v1.1001B
+// @version      v1.1002
 // @description  Making the best out of FurAffinity (aka. Furaffinity[dot]net) :3
 // @author       Nayra* & SoftyBnuny
 // @match        https://www.furaffinity.net/*
