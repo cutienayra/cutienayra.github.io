636617db3882c276e48b6186c4a546cfd490fb84
Nayra* - Lilac Pillow <nayracutie7@proton.me>
2025-01-20 17:43:03 +0000

    Update redm-tweaker.js
    Updated the code to fix bugs and update on Features :3

--- userscripts/redm-tweaker.js
+++ userscripts/redm-tweaker.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         RemovedEDM Tweaker~
-// @version      v1.3
+// @version      v2
 // @description  Making the best out of RemovedEDM :3
 // @author       Nayra* - Lilac Pillow
 // @match        https://removededm.com/*
@@ -19,7 +19,9 @@
     div.home-header>a[href="/Special:Search"],
     div.mw-cookiewarning-container
     #pt-betafeatures,
-    .mw-headline,
+    article#tabber-tabpanel-About-0.tabber__panel > center,
+    /* article#tabber-tabpanel-Videos-0.tabber__panel > center, */
+    .citizen-page-header-inner > .citizen-page-heading > .firstHeading-container, .citizen-page-header-inner > .citizen-page-heading > #siteSub,
     img[src="/w/thumb_handler.php/d/d0/default.jpg/900px-default.jpg"],
     div#footer-sitetitle>.mw-logo-wordmark,
     #dice-a,
@@ -27,11 +29,14 @@
     #citizen-pref-header,
     #citizen-search-trigger,
     #pt-userpage-username, a[href="/Special:Notifications"][title="Your notices"],
-    div#footer-sitetitle > .mw-logo-wordmark {
+    div#footer-sitetitle > .mw-logo-wordmark,
+    a.mw-file-description[style="cursor:auto"] > img.mw-file-element,
+    div.mw-content-ltr > h2.citizen-section-heading, div.mw-content-ltr > section#citizen-section-1.citizen-section,
+    div.mw-content-ltr > #citizen-section-0.citizen-section > hr {
     display:none!important;
 }
 h1.home-header__title::before,
-div#footer-sitetitle.mw-wiki-title {
+div#footer-sitetitle.mw-wiki-title::before {
   content: '"removed.edm"';
   content-visibility: visible;
 }
@@ -72,6 +77,37 @@ border: transparent!important;
 }
 #p-notifications.citizen-menu {
 margin-top: 0px1important;
+}
+
+h2.fwcp_cn > .mw-headline > font {
+font-size: 20px;
+display: block;
+margin-top: -5px;
+}
+.fwcp_fr > .b_factrow {
+  margin-top: -1.25px;
+  display: block;
+}
+.fwcp_ct > .fwcp_jcd {
+  margin-top: 8px;
+  display: block;
+}
+.fwcp_ctc > .fwcp_ct {
+  margin-top: -30px;
+}
+
+td.field_‎‎[style="background-image: repeating-linear-gradient(135deg, rgb(255, 165, 0), rgb(255, 165, 0) 10px, transparent 10px, transparent 20px); background-size: 20px 20px;"] {
+background-color: rgb(255, 165, 0)!important;
+}
+td.field_‎‎[style="background-image: repeating-linear-gradient(45deg, red, red 5px, transparent 5px, transparent 10px), repeating-linear-gradient(-45deg, red, red 5px, transparent 5px, transparent 10px); background-size: 20px 20px;"] {
+background-color: red!important;
+}
+
+article#tabber-tabpanel-Overview-0.tabber__panel > center,
+article#tabber-tabpanel-Videos-0.tabber__panel > center {
+  margin-top: 15px;
+  margin-bottom: -15px;
+  overflow: hidden!important;
 }
 ` );
 
