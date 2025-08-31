74d266ba46edd1123a7aea24a0456b9abaa98684
✮✿ Nayra* ✿✮ <nayracutie7@proton.me>
2025-08-31 22:32:28 +0100

    Update fa-tweaker.js
    Updating to the major version (Userscript overhaul)

--- userscripts/fa-tweaker.js
+++ userscripts/fa-tweaker.js
@@ -1,12 +1,9 @@
 // ==UserScript==
-// @name         FurAffinity Tweaker~
-// @version      v1.3
+// @name         FurAffinity[dot]net Tweaker~
+// @version      v2
 // @description  Making the best out of FurAffinity (aka. Furaffinity[dot]net) :3
-// @author       Nayra* & SoftyBnuny
-// @match        https://www.furaffinity.net/*
-// @match        https://furaffinity.net/*
-// @match        https://www.sfw.furaffinity.net/*
-// @match        https://sfw.furaffinity.net/*
+// @author       Nayra* aka. SoftyBnuny
+// @match        *://*.furaffinity.net/*
 // @run-at       document-start
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -14,24 +11,16 @@
 (function() {
     'use strict';
 
-function commitUserProfileCSSTweak() {
-    GM_addStyle ( `
-.news-block, [title="FA+"], [href="https://shop.furaffinity.net"][title="Shop"], [title="Shop"], div#footer, .leaderboardAd, .sidebarAds, .lileft[tabindex="0"], .lileft>.top-heading[href="#"], section.gallery-section:nth-child(3), section.gallery-section:nth-child(4), section.gallery-section:nth-child(5) {
-  display: none !important;
-}
+// Main CSS (All pages has to apply this CSS)
 
-#header.has-adminmessage {
-  height: 50px!important;
-  padding: 0px!important;
-  margin: 0px!important;
-}
-` );
-}
-
-if (window.location.pathname == '/') {
+function mainCSSload() {
 GM_addStyle ( `
-.news-block, [title="FA+"], [href="https://shop.furaffinity.net"][title="Shop"], [title="Shop"], div#footer, site-banner, .leaderboardAd, .sidebarAds, .lileft[tabindex="0"], .lileft>.top-heading[href="#"], section.gallery-section:nth-child(3), section.gallery-section:nth-child(4), section.gallery-section:nth-child(5) {
-  display: none !important;
+.news-block,
+[title="FA+"], [href="https://shop.furaffinity.net"][title="Shop"],
+[title="Shop"], div#footer, .leaderboardAd, .sidebarAds, .lileft[tabindex="0"],
+.lileft>.top-heading[href="#"], section.gallery-section:nth-child(3),
+section.gallery-section:nth-child(4), section.gallery-section:nth-child(5), site-banner {
+display: none !important;
 }
 
 #header.has-adminmessage {
@@ -39,63 +28,32 @@ GM_addStyle ( `
   padding: 0px!important;
   margin: 0px!important;
 }
-
-div#main-window > div#header {
-    height: 0px !important;
-    background: transparent !important;
-}
 ` );
+
+setTimeout(function(){
+console.clear();
+console.log(`♡ Thank you for using the FurAffinity[dot]net Tweaker ♡
+
+If there is any bugs involving this project, feel free to let Nayra* or SoftyBnuny know via. Discord!
+`);
+},5000);
 }
 
-else if (window.location.pathname.indexOf == '/browse') {
+// Individual page load (Important)
+
+if (document.location.pathname.indexOf("/user/") != -1 || document.location.pathname.indexOf("/gallery/") != -1 || document.location.pathname.indexOf("/scraps/") != -1 || document.location.pathname.indexOf("/favorites/") != -1 || document.location.pathname.indexOf("/journals/") != -1 || document.location.pathname.indexOf("/commissions/") != -1 || document.location.pathname.indexOf("/stats/") != -1) {
+mainCSSload();
 GM_addStyle ( `
-.news-block, [title="FA+"], [href="https://shop.furaffinity.net"][title="Shop"], [title="Shop"], div#footer, site-banner, .leaderboardAd, .sidebarAds, .lileft[tabindex="0"], .lileft>.top-heading[href="#"], div#main-window > div#header > site-banner {
-  display: none !important;
+site-banner {
+display: block !important;
 }
-
-#header.has-adminmessage {
-  height: 50px!important;
-  padding: 0px!important;
-  margin: 0px!important;
+userpage-nav-header {
+margin-top: -100px !important;
 }
-
-div#main-window > div#header {
-    height: 0px !important;
-    background: transparent !important;
-}
-` );
-}
-
-/* For user profile CSS */
-else if (document.location.pathname.indexOf("/user/") == 0) {
-commitUserProfileCSSTweak();
-}
-else if (document.location.pathname.indexOf("/gallery/") == 0) {
-commitUserProfileCSSTweak();
-}
-else if (document.location.pathname.indexOf("/scraps/") == 0) {
-commitUserProfileCSSTweak();
-}
-else if (document.location.pathname.indexOf("/favorites/") == 0) {
-commitUserProfileCSSTweak();
-}
-else if (document.location.pathname.indexOf("/commissions/") == 0) {
-commitUserProfileCSSTweak();
-}
-else if (document.location.pathname.indexOf("/stats/") == 0) {
-commitUserProfileCSSTweak();
+`);
 }
 
 else {
-GM_addStyle ( `
-.news-block, [title="FA+"], [href="https://shop.furaffinity.net"][title="Shop"], [title="Shop"], div#footer, .leaderboardAd, .sidebarAds, .lileft[tabindex="0"], .lileft>.top-heading[href="#"], div#main-window > div#header > site-banner {
-  display: none !important;
-}
-
-div#main-window > div#header {
-    height: 0px !important;
-    background: transparent !important;
-}
-` );
+mainCSSload();
 }
 })();
